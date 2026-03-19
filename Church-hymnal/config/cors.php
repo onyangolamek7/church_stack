dd475f2e10e105deb9d830871c40e26671a53989
onyangolamek7 <103209382+onyangolamek7@users.noreply.github.com>
2026-03-19 11:04:44 +0300

    Prepare for Railway Deployment

--- Church-hymnal/config/cors.php
+++ Church-hymnal/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://churchstack-production.up.railway.app'],
+    'allowed_origins' => ['http://localhost:4200'],
 
     'allowed_origins_patterns' => [],
 
--- church-frontend/src/environments/environment.prod.ts
+++ church-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://churchstack-production.up.railway.app'
+  apiUrl: '/api'
 };
