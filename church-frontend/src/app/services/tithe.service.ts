a844b08969dda3e04fddf17d07516969e1b91c04
onyangolamek7 <103209382+onyangolamek7@users.noreply.github.com>
2026-03-20 11:46:02 +0300

    fix: clear config cache on deploy

--- church-frontend/src/app/services/tithe.service.ts
+++ church-frontend/src/app/services/tithe.service.ts
@@ -1,44 +1,8 @@
-/*import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { CreateTitheDto } from '../models/create-tithe.dto';
-import { Tithe } from '../models/tithe.model';
-import { environment } from '../../environments/environment';
-
-export interface TitheResponse {
-  message: string;
-  tithe: {
-    id: number;
-    transaction_reference: string;
-    amount: number;
-    status: string;
-  };
-}
-
-@Injectable({
-  providedIn: 'root',
-})
-export class TitheService {
-  private apiUrl = `${environment.apiUrl}/tithes`;
-
-  constructor(private http: HttpClient) {}
-
-  getTithes(): Observable<Tithe[]> {
-    return this.http.get<Tithe[]>(this.apiUrl);
-  }
-
-  createTithe(data: CreateTitheDto): Observable<Tithe> {
-    return this.http.post<Tithe>(this.apiUrl, data);
-  }
-
-}*/
-
-
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, interval, switchMap, takeUntil, filter, take, Subject } from 'rxjs';
-import { environment } from '../../environments/environment';
 import { MpesaInitiateResponse, MpesaStatusResponse, MpesaTithePayload, TitheHistoryItem } from '../models/tithe.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class TitheService {
