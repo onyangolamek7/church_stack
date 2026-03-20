c8e761969ed90c57034fe1bc831ba561a34ea5a7
onyangolamek7 <103209382+onyangolamek7@users.noreply.github.com>
2026-03-20 13:52:38 +0300

    fixing cors

--- Church-hymnal/bootstrap/app.php
+++ Church-hymnal/bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin'=> \App\Http\Middleware\AdminMiddleware::class
         ]);
+        $middleware->api(prepend: [
+        \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
