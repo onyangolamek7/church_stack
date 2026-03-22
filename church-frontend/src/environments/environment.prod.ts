4b6d6711b474add32e79c82287a6335c0abaa4fd
onyangolamek7 <103209382+onyangolamek7@users.noreply.github.com>
2026-03-22 17:02:42 +0300

    fixation dockerfile and nginx

--- church-frontend/src/environments/environment.prod.ts
+++ church-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://churchstack-production-095e.up.railway.app/api'
+  apiUrl: 'https://churchstack-production-3bea.up.railway.app/api'
 };
